7293ef16061ae845945b3bcfdb975e0915d06998
hipstarcheat <125921081+hipstarcheat@users.noreply.github.com>
2024-12-12 02:51:27 +0300

    Delete CatalogItem.swift

--- CatalogItem.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Combine
-
-struct CatalogItem: Identifiable {
-    let id = UUID()          // Уникальный идентификатор
-    let title: String        // Название элемента (например, название бренда, модели или вкуса)
-    var children: [CatalogItem]? = nil  // Список дочерних элементов (например, модели для бренда)
-    var details: [String]? = nil // Дополнительная информация (данные из листа "Вкус" для модели)
-}
